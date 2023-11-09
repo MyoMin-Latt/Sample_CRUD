e91129b7cb50d1cc21bdb300cdf2bfc289d7d992
Kyaw Zayar Tun <kyawzayartun.sbs@gmail.com>
2023-11-09 13:29:36 +0630

    update > contact page

--- lib/core/presentation/router/app_router.gr.dart
+++ lib/core/presentation/router/app_router.gr.dart
@@ -12,29 +12,29 @@ import 'package:auto_route/auto_route.dart' as _i5;
 import 'package:flutter/material.dart' as _i7;
 import 'package:flutter_sample_crud/contact/domain/contact.dart' as _i6;
 import 'package:flutter_sample_crud/contact/presentation/add_contact_page.dart'
-    as _i1;
-import 'package:flutter_sample_crud/contact/presentation/contact_page.dart'
     as _i2;
+import 'package:flutter_sample_crud/contact/presentation/contact_page.dart'
+    as _i4;
 import 'package:flutter_sample_crud/contact/presentation/update_contact_page.dart'
     as _i3;
 import 'package:flutter_sample_crud/splash/presentation/splash_page.dart'
-    as _i4;
+    as _i1;
 
 abstract class $AppRouter extends _i5.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
   final Map<String, _i5.PageFactory> pagesMap = {
+    SplashRoute.name: (routeData) {
+      return _i5.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i1.SplashPage(),
+      );
+    },
     AddContactRoute.name: (routeData) {
       return _i5.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i1.AddContactPage(),
-      );
-    },
-    ContactRoute.name: (routeData) {
-      return _i5.AutoRoutePage<dynamic>(
-        routeData: routeData,
-        child: const _i2.ContactPage(),
+        child: const _i2.AddContactPage(),
       );
     },
     UpdateContactRoute.name: (routeData) {
@@ -47,17 +47,31 @@ abstract class $AppRouter extends _i5.RootStackRouter {
         ),
       );
     },
-    SplashRoute.name: (routeData) {
+    ContactRoute.name: (routeData) {
       return _i5.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i4.SplashPage(),
+        child: const _i4.ContactPage(),
       );
     },
   };
 }
 
 /// generated route for
-/// [_i1.AddContactPage]
+/// [_i1.SplashPage]
+class SplashRoute extends _i5.PageRouteInfo<void> {
+  const SplashRoute({List<_i5.PageRouteInfo>? children})
+      : super(
+          SplashRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'SplashRoute';
+
+  static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i2.AddContactPage]
 class AddContactRoute extends _i5.PageRouteInfo<void> {
   const AddContactRoute({List<_i5.PageRouteInfo>? children})
       : super(
@@ -66,20 +80,6 @@ class AddContactRoute extends _i5.PageRouteInfo<void> {
         );
 
   static const String name = 'AddContactRoute';
-
-  static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
-}
-
-/// generated route for
-/// [_i2.ContactPage]
-class ContactRoute extends _i5.PageRouteInfo<void> {
-  const ContactRoute({List<_i5.PageRouteInfo>? children})
-      : super(
-          ContactRoute.name,
-          initialChildren: children,
-        );
-
-  static const String name = 'ContactRoute';
 
   static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
 }
@@ -123,15 +123,15 @@ class UpdateContactRouteArgs {
 }
 
 /// generated route for
-/// [_i4.SplashPage]
-class SplashRoute extends _i5.PageRouteInfo<void> {
-  const SplashRoute({List<_i5.PageRouteInfo>? children})
+/// [_i4.ContactPage]
+class ContactRoute extends _i5.PageRouteInfo<void> {
+  const ContactRoute({List<_i5.PageRouteInfo>? children})
       : super(
-          SplashRoute.name,
+          ContactRoute.name,
           initialChildren: children,
         );
 
-  static const String name = 'SplashRoute';
+  static const String name = 'ContactRoute';
 
   static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
 }
